f95999822c95ca289cda442d592e2516e59f1be7
Centtiii <143886786+Centtiii@users.noreply.github.com>
2023-11-10 22:28:01 +0800

    Update web.php

--- activity5/routes/web.php
+++ activity5/routes/web.php
@@ -21,6 +21,9 @@ Auth::routes();
 Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
 Route::get('/registration', 'RegistrationController@index')->name('registration');
 
+Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('/login', 'Auth\LoginController@login');
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
